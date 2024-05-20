5721495ce619b30d062c6b936ec193a3c625eaca
jokasoa <alexnovoa168@gmail.com>
2024-05-20 08:05:23 -0500

    Agrega este Proyecto

---
+++ MayorEdad.psc
@@ -0,0 +1,9 @@
+Algoritmo MayorEdad
+	Escribir "Ingresa tu edad"
+	Leer edad
+	Si edad>18 Entonces
+		Escribir " Es mayor de edad"
+	SiNo
+		
+	FinSi
+FinAlgoritmo
---
+++ edad.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout << "Ingresa tu edad";
+    int edad; //edad es una variable del tipo entero
+    cout << "Ingresa la edad";
+    cin>>edad;
+    if (edad> 18)
+    {
+    cout<<"eres mayor de edad";
+    cout<<endl<"Bienvenido";
+    }
+
+    return 0;
+}
