b284cdf207009b10623d970f696356f6aa607449
jokasoa <alexnovoa168@gmail.com>
2024-05-20 09:04:04 -0500

    Agrega este Proyecto

---
+++ Num.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std
+
+int main() {
+    // Write C++ code here
+    int num1,num2;
+    cout <<"Agrege el primer numero" ;
+    cin >>num1;
+    if (num1>=num2)
+    cout <<"Los numeros son iguales" ;
+    std::cout << "Try programiz.pro";
+    else
+    {
+        count <<"el mayor es: "<<num1;
+        count <<endl<<"el menor es: "<<num2;
+    }
+    else
+}
+
+    return 0;
